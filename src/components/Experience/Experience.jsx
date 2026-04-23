1bed80703bbd28e6a931f9b1a48ac26a0570ba44
Dēivid Sans <david.mancillas@icloud.com>
2026-04-22 22:27:33 -0600

    primer borrador

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+import SectionHeading from "../SectionHeading/SectionHeading";
+import { Icon } from "@iconify/react";
+
+const Experience = ({ data }) => {
+  const { text, experience, resumeCv } = data;
+  return (
+    <section id="experience" className="section experience-section bg-g">
+      <div className="container">
+        <div className="row gy-5">
+          <div className="col-lg-5">
+            <div className="section-heading">
+              <SectionHeading title="Work Experience" subTitle="" />
+              <p
+                data-aos="fade-up"
+                data-aos-duration="800"
+                data-aos-delay="300">
+                {text}
+              </p>
+              <div
+                className="btn-bar"
+                data-aos="fade-up"
+                data-aos-duration="800"
+                data-aos-delay="200">
+                <a href={resumeCv} className="px-btn dark" download>
+                  Download my resume <Icon icon="bi-download" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-7 ps-xl-5">
+            <ul className="resume-box">
+              {experience.map((element, index) => (
+                <li key={index} data-aos="fade-up" data-aos-duration="800">
+                  <div className="r-meta">
+                    <span>
+                      -{element.start} - {element.end}
+                    </span>
+                    <label>-{element.subtitle}</label>
+                  </div>
+                  <h5>{element.title}</h5>
+                  <ol style={{ listStyleType: "disc" }}>
+                    <li>{element.description_line_1}</li>
+                    <span>
+                      <li>{element.description_line_2}</li>
+                    </span>
+                  </ol>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+Experience.propTypes = {
+  data: PropTypes.object,
+};
+
+export default Experience;
